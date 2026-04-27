d12f49e62d065e526a5d5f0083468bc838a0abd8
Anywhay <timursaripov03480@gmail.com>
2026-04-28 02:12:07 +0300

    readme + nvim

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -14,4 +14,121 @@ return require("lazy").setup({
       -- })
     end
   },
+  -- Status Bar
+  {
+    'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function()
+      require('lualine').setup({
+        options = { 
+          theme = 'auto', 
+          globalstatus = true, 
+        },
+        sections = {
+          lualine_a = { 'mode' },
+          lualine_b = { 'branch', 'diff', 'diagnostics' },
+          lualine_c = { { 'filename', path = 1 } }, 
+          lualine_x = { 'encoding', 'filetype' },
+          lualine_y = { 'progress' },
+          lualine_z = { 'location' }
+        },
+      })
+    end
+  },
+  -- Tabs
+  {
+    'akinsho/bufferline.nvim',
+    version = "*",
+    dependencies = 'nvim-tree/nvim-web-devicons',
+    config = function()
+      require("bufferline").setup({
+        options = {
+          mode = "buffers", 
+          separator_style = "thick", 
+          show_close_icon = false, 
+          always_show_bufferline = true,
+          offsets = {
+            {
+              filetype = "NvimTree",
+              text = "File Explorer",
+              text_align = "left",
+              separator = true,
+            }
+          },
+        }
+      })
+    end
+  },
+  -- Auto complit
+  {
+    'hrsh7th/nvim-cmp',
+    dependencies = {
+      'hrsh7th/cmp-nvim-lsp',     
+      'hrsh7th/cmp-buffer',       
+      'hrsh7th/cmp-path',         
+      'L3MON4D3/LuaSnip',         
+      'saadparwaiz1/cmp_luasnip', 
+    },
+    config = function()
+      local cmp = require('cmp')
+      local luasnip = require('luasnip')
+
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            luasnip.lsp_expand(args.body)
+          end,
+        },
+        mapping = cmp.mapping.preset.insert({
+          ['<C-k>'] = cmp.mapping.select_prev_item(), 
+          ['<C-j>'] = cmp.mapping.select_next_item(), 
+          ['<C-e>'] = cmp.mapping.abort(),           
+          ['<CR>'] = cmp.mapping.confirm({ select = true }), 
+        }),
+        sources = cmp.config.sources({
+          { name = 'nvim_lsp' }, 
+          { name = 'luasnip' },  
+          { name = 'buffer' },   
+          { name = 'path' },     
+        }),
+      })
+    end
+  },
+
+  -- Snippets
+  {
+    'L3MON4D3/LuaSnip',
+    dependencies = { 'rafamadriz/friendly-snippets' },
+    config = function()
+      require("luasnip.loaders.from_vscode").lazy_load()
+    end,
+  },
+  -- File tree
+  {
+    'nvim-tree/nvim-tree.lua',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function()
+      require('nvim-tree').setup()
+      vim.keymap.set('n', '<leader>t', ':NvimTreeToggle<CR>', { silent = true })
+    end
+  },
+  -- Улучшенная подсветка синтаксиса (Treesitter)
+  {
+    'nvim-treesitter/nvim-treesitter',
+    build = ':TSUpdate',
+    config = function()
+      require('nvim-treesitter.configs').setup({
+        -- Список языков, которые нужно установить сразу
+        ensure_installed = { "lua", "python", "rust", "bash", "javascript", "markdown", "markdown_inline" },
+        
+        highlight = {
+          enable = true, -- Включить саму подсветку
+          additional_vim_regex_highlighting = false,
+        },
+        indent = {
+          enable = true, -- Улучшенные автоматические отступы
+        },
+      })
+    end,
+  },
 })
---
+++ README.md
@@ -0,0 +1,78 @@
+# My dotfiles
+
+In this repository I want to share with you my dotfiles that form the basis of my rice.The key idea of this rice is minimalism and neatness.
+
+
+## Features of the project
+
+- **Easy installation**: To get started quickly, clone the repository and use the GNU Stow utility to create the necessary symlinks.
+- **Compatibility**: Works flawlessly regardless of the technical components of the device.
+
+## Screenshot gallery
+
+![linux_brainrot](/home/anywhay/Pictures/Screenshots/rice/2026-04-28_01-37-38.png)
+![nvim](/home/anywhay/Pictures/Screenshots/rice/22026-04-28_01-39-24.png)
+![terminal](/home/anywhay/Pictures/Screenshots/rice/2026-04-28_01-40-09.png)
+![thunar](/home/anywhay/Pictures/Screenshots/rice/2026-04-28_01-40-57.png)
+![rofi](/home/anywhay/Pictures/Screenshots/rice/2026-04-28_01-44-30.png)
+![poweroffmenu](/home/anywhay/Pictures/Screenshots/rice/2026-04-28_01-45-11.png)
+
+## Main components
+
+- **Window manager**: Hyprland
+- **Display manager**: Sddm
+- **Terminal**: Kitty
+- **Shell**: Zsh
+- **Lock screen**: Hyprlock
+- **Walpaper backend**: awww
+- **Colorpicker**: Hyprpicker
+- **Screenshot**: Grim + Slurp + Swappy
+- **File manager**: Thunar
+- **Power menu, app launcher**: Rofi
+- **Taskbar**: Waybar
+- **Notifications**: Swaync
+- **Fetch**: Fastfetch
+
+## Compatibility
+
+### Linux distribution
+
+As for now, this installer is only made for Arch and its-based distributions. Support of any other distributions is not planned yet.
+
+### Device type
+
+- Desktop ✅
+- Laptop ✅
+
+### GPU vendor
+
+- AMD ✅ (not tested)
+- Nvidia ✅
+- Intel ✅
+
+## How to install
+
+> Installation can take from 10 to 30 minutes with archinstall or 5 to 15 without. Correction: This repository is not a full-fledged project. It only stores dotfiles. To use them successfully, you need uset uttility "GNU Stow"
+
+### 1. Install Git, Stow
+
+```
+sudo pacman -S git, stow
+```
+### 2. Сlone repository
+
+```
+git clone https://github.com/TimurSharipovv/hyprland_dots.git 
+```
+### 3. Move to dotfiles
+
+```
+cd dotfiles
+```
+### 4. Create simlinks with stow.
+
+```
+stow .
+```
+
+
